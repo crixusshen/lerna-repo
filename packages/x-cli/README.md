c28dda1c1beef1dc8cec1a9ff2590c3558205bd1
dfzq <dfzq@MacBookPro.local>
2018-09-04 10:45:20 +0800

    no message

---
+++ packages/x-cli/README.md
@@ -0,0 +1,3 @@
+#### This is a test lerna project
+
+x-cli extend x-core; x-core extend other packages
--- packages/x-cli/src/main.ts
+++ packages/x-cli/src/main.ts
@@ -2,5 +2,6 @@ import { awesomeFn } from "@crixusshen/x-core";
 
 export function cli() {
   awesomeFn();
+  console.log(1)
   return Promise.resolve(true);
 }
